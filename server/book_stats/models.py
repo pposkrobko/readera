a491c2a69228f14cb168a10992277ca0c93cb467
Michal Waszczuk <mw361260@mimuw.edu.pl>
2016-04-25 22:01:17 +0200

    Models revision.

--- server/book_stats/models.py
+++ server/book_stats/models.py
@@ -16,6 +16,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    max_pages = models.IntegerField()
 
 
 class BookStats(models.Model):
@@ -23,9 +24,16 @@ class BookStats(models.Model):
     user = models.ForeignKey(User)
     on_page = models.IntegerField(default=0)
     reading_time = models.DurationField(default = datetime.timedelta(seconds = 1))
+    last_time_used = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class BookStatsHistory(models.Model):
+    book_stats = models.ForeignKey(BookStats, on_delete=models.CASCADE)
+    time = models.DateField(auto_now=True)
+    pages_read = models.IntegerField()
 
 
 # Create your models here.
