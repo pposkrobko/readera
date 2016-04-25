0b477f9b9f6f2733c059d9611134520ac9bb4fb1
Michal Waszczuk <mw361260@mimuw.edu.pl>
2016-04-25 21:30:16 +0200

    Applied migrations.

--- server/book_stats/models.py
+++ server/book_stats/models.py
@@ -6,25 +6,26 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+
 class BookStats(models.Model):
     book = models.ForeignKey(Book, on_delete = models.CASCADE)
     user = models.ForeignKey(User)
     on_page = models.IntegerField(default=0)
     reading_time = models.DurationField(default = datetime.timedelta(seconds = 1))
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
-
-class Author:
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
 
 
 # Create your models here.
