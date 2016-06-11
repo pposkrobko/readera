52542801b9321ccd23ab431120e34754e91ffc03
Michal Waszczuk <mw361260@mimuw.edu.pl>
2016-06-11 14:20:03 +0200

    User profile refactor

--- server/book_stats/urls.py
+++ server/book_stats/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+from book_stats import views
 
 urlpatterns = [
-    url(r'^user/', login_required(TemplateView.as_view(template_name="book_stats/user.html")), name ="profile"),
+    url(r'^user/', login_required(views.ProfileView.as_view()), name ="profile"),
     url(r'^stat/', login_required(TemplateView.as_view(template_name="book_stats/stats.html")), name = "stats"),
 ]
--- server/book_stats/views.py
+++ server/book_stats/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from book_stats.models import BookStats
+
+
+class ProfileView(ListView):
+    template_name = "book_stats/user.html"
+
+    def get_queryset(self):
+        return BookStats.objects.filter(user=self.request.user).filter(state=BookStats.IN_PROGRESS)
+
 
 # Create your views here.
